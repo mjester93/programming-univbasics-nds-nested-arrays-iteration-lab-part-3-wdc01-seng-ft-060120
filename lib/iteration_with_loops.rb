b7d5243ca5ad6c5aa3521ef1cef05c94e974fd45
Michael Jester <m.jester.93@gmail.com>
2020-05-22 22:33:12 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ def join_nested_strings(src)
   while counter < src.length do
     inner_counter = 0
     while inner_counter < src[counter].length do
-      if src[counter][inner_counter].class == "String"
+      if src[counter][inner_counter].class == String
         string_array.push(src[counter][inner_counter])
       end
 
